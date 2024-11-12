d78ad60cb9eb90e884e0059f25a7e0bc8c1117d3
Sergey <90494834+makarenkoooooo@users.noreply.github.com>
2024-11-12 18:33:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,28 @@ const num1 = document.getElementById("num1");
 const num2 = document.getElementById("num2");
 const operator = document.getElementById("operator");
 const equalsBtn = document.getElementById("equals-btn");
-const result = document.getElementById("result");
 
 function operationResult() {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("result");
+
+  // Проверяем оператор и выполняем соответствующую операцию
   if (operator.value === "+") {
-    result.textContent = +num1.value + +num2.value;
+    newDiv.textContent = +num1.value + +num2.value;
   } else if (operator.value === "-") {
-    result.textContent = +num1.value - +num2.value;
+    newDiv.textContent = +num1.value - +num2.value;
   } else if (operator.value === "*") {
-    result.textContent = +num1.value * +num2.value;
+    newDiv.textContent = +num1.value * +num2.value;
   } else if (operator.value === "/") {
-    result.textContent = +num1.value / +num2.value;
+    newDiv.textContent = +num1.value / +num2.value;
   }
+
+  // Добавляем обработчик события для удаления `div` при клике
+  newDiv.addEventListener("click", () => {
+    newDiv.remove();
+  });
+
+  document.body.appendChild(newDiv);
 }
 
 equalsBtn.addEventListener("click", operationResult);
